5a6d41127e1ca8df605c9c323f63b92f0ed51788
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-06-21 17:24:16 +0700

    race condition

---
+++ 119_race_condition_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestRaceCondition(t *testing.T) {
+	x := 0
+
+	// GoRoutine with race condition
+	for i := 1; i <= 1000; i++ {
+		go func ()  {
+			for j := 1; j <= 100; j++ {
+				x = x + 1
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("Counter", x)
+}
