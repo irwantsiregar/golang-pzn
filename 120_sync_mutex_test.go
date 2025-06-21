04c158962b9ea253a3bed3ce96789d59ed77aa23
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-06-21 17:51:09 +0700

    sync mutex: for lock & unlock goroutine process

---
+++ 120_sync_mutex_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestSyncMutex(t *testing.T) {
+	x := 0
+
+	// For lock & unlock goroutine process
+	var mutex sync.Mutex
+
+	// GoRoutine with race condition
+	for i := 1; i <= 1000; i++ {
+		go func ()  {
+			for j := 1; j <= 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter", x)
+}
