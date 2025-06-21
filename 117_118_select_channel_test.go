3e0b972d4d5cf90a1eb1371c8dd34bf4d4b10b33
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-06-21 17:05:03 +0700

    default select channel

--- 117_118_select_channel_test.go
+++ 117_118_select_channel_test.go
@@ -41,3 +41,35 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestSelectDefdaultChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponses(channel1)
+	go GiveMeResponses(channel2)
+	
+	counter := 0
+
+	// For with infinity loop
+	for {
+		select {
+		case data := <- channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++	
+		case data := <- channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data") // Display when waiting channel process
+		}
+
+		// Counter for break loop
+		if counter == 2 {
+			break	
+		}
+	}
+}
