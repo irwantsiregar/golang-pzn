02450a2186bce8c0812d0621ea47eff1466e2df4
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-06-22 23:13:21 +0700

    finished: goroutine

--- 128_sync_atomic_test.go
+++ 128_sync_atomic_test.go
@@ -14,7 +14,7 @@ func TestAtomic(t *testing.T) {
 	// Handle race condition with Atomic. Ex: for data type primitif
 	for i := 1; i <= 1000; i++ {
 		go func ()  {
-			// group.Add(1)
+			group.Add(1)
 			for j := 1; j <= 100; j++ {
 				atomic.AddInt64(&x, 1)
 			}
--- 131_gomaxprocs_test.go
+++ 131_gomaxprocs_test.go
@@ -11,6 +11,7 @@ import (
 func TestGetGomaxprocs(t *testing.T) {
 	group := sync.WaitGroup{}
 
+	// Make goroutine any 100 and 2 built in. (100 + 2 = 102 goroutine)
 	for i := 0; i < 100; i++ {
 		group.Add(1)
 
@@ -20,14 +21,14 @@ func TestGetGomaxprocs(t *testing.T) {
 		}()
 	}
 
-	totalCpu := runtime.NumCPU()
+	totalCpu := runtime.NumCPU() // Total CPU
 	fmt.Println("Total CPU", totalCpu)
 
 	runtime.GOMAXPROCS(20) // For UPDATE total thread
-	totalThread := runtime.GOMAXPROCS(-1)  // default check is value -1, for update use value >0
+	totalThread := runtime.GOMAXPROCS(-1)  // Default check thread is value -1, for update use value >0
 	fmt.Println("Total Thread", totalThread)
 
-	totalGoroutine := runtime.NumGoroutine()
+	totalGoroutine := runtime.NumGoroutine() // Total GoRoutine
 	fmt.Println("Total Goroutine", totalGoroutine)
 	
 	group.Wait()
